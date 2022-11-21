fa46e36536a03ab5b20529cb906ccb6529eef59e
Virgile Landeiro <virgile.landeiro@gmail.com>
2022-11-20 16:23:25 -0800

    refactored using command and formatted strings

--- pytransient/app.py
+++ pytransient/app.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 from typing import Callable
 from uuid import uuid4
-
+from pathlib import Path
 import click
 import structlog
 from rich.table import Table
@@ -16,18 +16,23 @@ from textual.reactive import reactive
 from textual.screen import Screen
 from textual.widget import Widget
 from textual.widgets import Button, Footer, Header, Input, TextLog
+from textual import log
 
 from .action import Action, Argument
+from .models import Transient, Argument, Command, Flag
 from .config import Config
 from .state_machine import StateMachine
 
-log = structlog.get_logger()
+# log = structlog.get_logger()
 
 
-class InputScreen(Screen):
-    def __init__(self, argument, *args, **kwargs):
-        self.argument = argument
-        self.user_input = Input(id="user-input", placeholder="value")
+class InputValueSetter(Screen):
+    def __init__(self, on_success, on_empty, *args, **kwargs):
+        self.on_success = on_success
+        self.on_empty = on_empty
+        placeholder = kwargs.pop("placeholder", "value")
+        self.accept_null_value = kwargs.pop("accept_null_value", True)
+        self.user_input = Input(id="user-input", placeholder=placeholder)
         super().__init__(*args, **kwargs)
 
     def compose(self) -> ComposeResult:
@@ -35,8 +40,14 @@ class InputScreen(Screen):
 
     def on_key(self, event: events.Key) -> None:
         if event.key == "enter":
-            self.argument.value = self.user_input.value
-            self.argument.toggle(True)
+            if not self.user_input.value and not self.accept_null_value:
+                return
+
+            if self.user_input.value:
+                self.on_success(self.user_input.value)
+            else:
+                self.on_empty(self.user_input.value)
+
             event.stop()
             self.app.pop_screen()
 
@@ -45,20 +56,101 @@ class InputScreen(Screen):
             self.app.pop_screen()
 
 
-class CLIConfigWidget(Widget):
+class TransientKeymapWidget(Widget):
+    transient = reactive(Transient.empty())
+    input_buffer = reactive("")
+    stack = []
 
-    def __init__(self, action: Action, value_setter: Callable, *args, **kwargs):
-        self.state_machine = StateMachine.from_action(action, value_setter)
-        super().__init__(*args, **kwargs)
+    def argument_value_setter(
+            self,
+            argument: Argument,
+            placeholder="value",
+            accept_null_value=True,
+            send_command=False
+    ):
+
+        def on_success(new_value):
+            argument.value = new_value
+            argument.is_active = True
+
+        def on_empty(new_value):
+            argument.value = ""
+            argument.is_active = False
+
+        input_screen = InputValueSetter(
+            on_success=on_success,
+            on_empty=on_empty,
+            placeholder=placeholder,
+            accept_null_value=accept_null_value
+        )
+
+        self.app.push_screen(input_screen)
+        input_screen.user_input.focus()
 
     def on_key(self, event: events.Key) -> None:
-        self.state_machine.apply(event.key, event.char)
+        key, char = event.key, event.char
 
-    def generate_command(self) -> str:
-        return self.state_machine.generate_command()
+        self.transient.highlighted_keys = set()
+
+        # Backtrack to the previous state
+        if key == "ctrl+g":
+            if len(self.stack):
+                log("Tracking back")
+                self.transient = self.stack.pop(-1)
+                self.input_buffer = ""
+            else:
+                self.app.exit()
+            return
+
+        if not char:
+            return
+
+        self.input_buffer += char
+
+        # Handle matching user input
+        if self.input_buffer in self.transient.keymap:
+            target = self.transient.keymap[self.input_buffer]
+
+            if isinstance(target, Transient):
+                self.stack.append(self.transient)
+                self.transient = target
+
+            elif isinstance(target, Flag):
+                target.is_active = not target.is_active
+
+            elif isinstance(target, Argument):
+                self.argument_value_setter(target)
+
+            elif isinstance(target, Command):
+                send_command = True
+                for parameter in target.parameters:
+                    if not parameter.is_optional and not parameter.param.is_active:
+                        self.transient.highlighted_keys.add(parameter.param.key)
+                        send_command = False
+
+                if send_command:
+                    self.app.exit(target.to_list())
+
+            self.input_buffer = ""
+            self.transient.disabled_keys = set()
+
+            log(self.transient.highlighted_keys)
+        else:
+            # Disable keys that don't match the input buffer
+            for key in self.transient.keymap:
+                if key in self.transient.disabled_keys:
+                    continue
+                if not key.startswith(self.input_buffer):
+                    self.transient.disabled_keys.add(key)
+
+            # Reset input buffer and disabled keys when no remaining keys match the
+            # user input
+            if set(self.transient.keymap) == self.transient.disabled_keys:
+                self.transient.disabled_keys = set()
+                self.input_buffer = ""
 
     def render(self):
-        return self.state_machine
+        return self.transient
 
 
 class PyTransientApp(App):
@@ -69,39 +161,21 @@ class PyTransientApp(App):
             key="ctrl+g",
             key_display="CTRL+G",
             action="do_nothing_1",
-            description="Go back",
-        ),
-        Binding(
-            key="enter",
-            key_display="ENTER",
-            action="do_nothing_2",
-            description="Run the command",
+            description="Go back / Exit",
         ),
     ]
 
-    def __init__(self, command, *args, **kwargs):
-        self.command: Action = command
+    def __init__(self, transient, *args, **kwargs):
+        self.transient = transient
         super().__init__(*args, **kwargs)
 
-    def on_key(self, event: events.Key) -> None:
-        if event.key == "enter":
-            cmd = self.config_widget.generate_command()
-            self.app.exit(cmd)
-
-        self.config_widget.on_key(event)
-        self.config_widget.refresh()
+    def on_key(self, event):
+        self.transient_widget.on_key(event)
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
+        self.transient_widget = TransientKeymapWidget()
+        self.transient_widget.transient = self.transient
 
-        def value_setter(arg):
-            input_screen = InputScreen(arg)
-            self.push_screen(input_screen)
-            input_screen.user_input.focus()
-
-        self.config_widget = CLIConfigWidget(
-            self.command, value_setter, id="command-selection"
-        )
-
-        yield self.config_widget
+        yield self.transient_widget
         yield Footer()
--- pytransient/cli.py
+++ pytransient/cli.py
@@ -5,33 +5,22 @@ import click
 import structlog
 
 from .app import PyTransientApp
-from .config import Config
+from .models import Transient
 
 log = structlog.get_logger()
 
+
 @click.command()
 @click.argument("command-name")
-@click.option("--entry-point")
-def main(command_name: str, entry_point: str = None):
-    config = Config.from_command_name(command_name)
-    entry_point = entry_point or command_name
-
-    if entry_point not in config.actions:
-        log.error(
-            f"Action '{entry_point}' is not available in the loaded config. "
-            "With this config file, you must use one of the following actions: "
-            f"{set(config.keys())}."
-        )
-        sys.exit(1)
-
-    action = config.actions[entry_point]
-    app = PyTransientApp(action)
+def main(command_name: str):
+    transient = Transient.from_command_name(command_name)
+    app = PyTransientApp(transient)
     cmd = app.run()
 
     if cmd:
         cmd_str = " ".join(cmd)
         print(f"Running '{cmd_str}'")
-        subprocess.run(["echo", *cmd])
+        subprocess.run(cmd)
 
 
 if __name__ == "__main__":
