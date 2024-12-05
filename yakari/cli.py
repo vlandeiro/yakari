9d11b506ca4e09d5b62d2b26043a62a0a35d78a8
Virgile Landeiro <virgile.landeiro@gmail.com>
2024-12-05 15:09:46 -0800

    add -d short option from dry run

--- yakari/cli.py
+++ yakari/cli.py
@@ -8,6 +8,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("command_name", help="Name of the command to execute")
     parser.add_argument(
+        "-d",
         "--dry-run",
         action="store_true",
         help="If toggled, Yakari only prints the command rather than running it.",
